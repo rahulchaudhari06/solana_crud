c819cec18552835285baa48403ba91b2ae8384b1
Rahul Chaudhari_ <rahulgc545@gmail.com>
2024-11-24 14:19:34 +0530

    init

--- anchor/programs/basic/src/lib.rs
+++ anchor/programs/basic/src/lib.rs
@@ -1,16 +1,109 @@
+#![allow(clippy::result_large_err)]
+
 use anchor_lang::prelude::*;
 
 declare_id!("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNNF");
 
 #[program]
-pub mod basic {
+pub mod journal {
+
+
     use super::*;
 
-    pub fn greet(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("GM!");
+    pub fn create_entry(
+        ctx: Context<CreateEntry>,
+        title: String,
+        message: String,
+    ) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.owner = ctx.accounts.owner.key();
+        journal_entry.title = title;
+        journal_entry.message = message;
+
         Ok(())
     }
+
+    pub fn update_entry(
+        ctx: Context<CreateEntry>,
+        _title: String,
+        new_message: String,
+    ) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.message = new_message;
+
+        Ok(())
+    }
+
+    pub fn delete_entry(
+       _ctx: Context<DeleteEntry>,
+       title: String,
+    ) -> Result<()> {
+        Ok(())
+    }
+    
+    
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState{
+    pub owner: Pubkey,
+    #[max_len(20)]
+    pub title: String,
+    #[max_len(200)]
+    pub message: String,
+    pub entry_id: u64,
+}
+
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(title: String)]
+pub struct CreateEntry<'info> {
+    #[account(
+        init,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        payer = owner,
+        space = 8 + JournalEntryState::INIT_SPACE,
+    )]
+
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>, 
+}   
+
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct UpdateEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        realloc = 8 + JournalEntryState::INIT_SPACE,
+        realloc::payer = owner,
+        realloc::zero = true,
+
+    )]
+
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>, 
+}   
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteEntry<'info>{
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        close = owner,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>, 
+}
